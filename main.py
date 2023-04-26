0582a343c20ac37f50bf2ca125e8cab9d23eed44
Gaia Di Lorenzo <gaid@bendingspoons.com>
2023-04-26 10:17:44 +0200

    feat: clean main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import multiprocessing as mp
 from modules.compiler import Compiler
 from modules.data_loader import DataLoader
 from modules.arg_parser import ArgParser
+from modules.fuzzer import Fuzzer
 
 def main():
     arg_parser = ArgParser()
@@ -13,34 +14,15 @@ def main():
     compiler = Compiler(args)
     data_loader = DataLoader()
     
+    tests = data_loader.tests()
+    
     if not os.path.exists('data'):
         os.makedirs('data')
     
-    #TODO: change this - for now it just takes the c files in the tests directory
-    files = [ test.name for test in data_loader.tests()]
+    fuzzer = Fuzzer(tests=tests, compiler=compiler, num_cores=args.num_cores, n_threshold=args.n_threshold)
+    fuzzer.fuzz()
     
-    interesting_tests = []
-    # use mp pool to run the tests in parallel
-    with mp.Pool(args.num_cores) as pool:
-        with tqdm(total=len(files)) as pbar:
-            for stats in pool.imap_unordered(compiler.compile_test, files):
-                pbar.update()
-                if stats is None:
-                    continue
-                #pop file for the check and then add it back
-                file = stats.pop("file")
-                if not all(value == stats["last"] for value in stats.values()):
-                    stats["file"] = file
-                    pbar.set_description_str(str(stats))
-                    interesting_tests.append(stats)
-            pbar.close()
-
-    for i in interesting_tests:
-        min_v = min([value for key, value in i.items() if not key.startswith("file") and not key.startswith("last")])
-        i["max_rateo"] = round(i["last"] / min_v, 2)
-        if i["max_rateo"] > 1:
-            print(i)
-            json.dump(i, open(os.path.join("data", i["file"] + ".json"), "w"))
-
+    
+    
 if __name__ == "__main__":
     main()
